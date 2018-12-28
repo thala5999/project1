fe7c3858c35e729ce53f3b2e5c641a0580d26cc9
UmaMahesh <UmaMahesh@192.168.0.176>
2018-12-28 11:47:03 +0530

    addd

---
+++ project1/src/project1/Pro1.java
@@ -0,0 +1,18 @@
+package project1;
+
+public class Pro1 {
+public static void main(String[] args) {
+
+
+
+Hello h=new Hello();
+h.m1();
+}}
+class Hello{
+	int a;
+	int b;
+	void m1() {
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
